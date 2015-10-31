8a02b369db84be4d9b6c0cc4d14daf959533f00a
Nick Landolfi <nclandolfi@gmail.com>
2015-10-31 15:53:48 -0700

    cleanup session file

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -2,25 +2,45 @@ part of core.models;
 
 const String SessionKind = "session";
 const String SessionSpace = "sessions";
+Session NewSession(Map<String, dynamic> s) => new Session.fromStructure(s);
 
 class Session extends Model {
+
+  // --- Properties {{{
+
   String token;
   int expires_after;
   String owner_id;
   String credential_id;
 
+  // --- }}}
+
+  // --- Record Implementation {{{
   String Kind() {
     return SessionKind;
   }
 
   Session.fromStructure(Map<String, dynamic> s) {
-      loadBase(s);
+    loadBase(s);
 
     this.token = s['token'];
     this.expires_after = s['expires_after'];
     this.owner_id = s['owner_id'];
     this.credential_id = s['credential_id'];
   }
+
+  Map<String, dynamic> Structure() {
+    return {
+      "id": this.id,
+      "token": this.token,
+      "expires_after": this.expires_after,
+      "owner_id": owner_id,
+      "credential_id": this.credential_id,
+    };
+  }
+  // --- }}}
+
+  // --- Static Methods (find, authenticate) {{{
 
   static Future<Session> find(data.DB db, String id) {
     return db.Find(SessionKind, id) as Future<Session>;
@@ -32,23 +52,20 @@ class Session extends Model {
     var data = JSON.encode({"public": public, "private": private});
 
     HttpRequest
-        .request(host + "/sessions?public=$public&private=$private",
-            method: "POST", sendData: data)
-        .then((req) {
-      if (req.status == 200 || req.status == 201) {
-        Map<String, dynamic> s = JSON.decode(req.response);
-        completer.complete(new Session.fromStructure(s["data"]["session"]));
-      } else {
-        completer.completeError("shoot");
-      }
-    });
+      .request(host + "/sessions?public=$public&private=$private",
+          method: "POST", sendData: data)
+      .then((req) {
+        if (req.status == 200 || req.status == 201) {
+          Map<String, dynamic> s = JSON.decode(req.response);
+          completer.complete(new Session.fromStructure(s["data"]["session"]));
+        } else {
+          completer.completeError("shoot");
+        }
+      });
 
     return completer.future;
   }
 
-  Map<String, dynamic> Structure() {
-    return {"id": this.id, "token": this.token};
-  }
-}
+  // --- }}}
 
-Session NewSession(Map<String, dynamic> s) => new Session.fromStructure(s);
+}
