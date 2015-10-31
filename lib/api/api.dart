91609c1214483d2fd816f2cf8ae98508e246623b
Nick Landolfi <nclandolfi@gmail.com>
2015-10-31 16:00:24 -0700

    break out api lib

---
+++ lib/api/api.dart
@@ -0,0 +1,36 @@
+library core.api;
+
+import "dart:async" show Future;
+import "dart:convert" show JSON;
+import "dart:html" show HttpRequest;
+
+import "package:data/data.dart" as data;
+
+const ElosAuthHeader = "Elos-Auth";
+
+class Host implements data.Host {
+  final String host;
+  final String accessToken;
+
+  Host(this.host, this.accessToken);
+
+  Future<HttpRequest> request(String method, String url, dynamic data) async {
+    print("${method} on ${host}${url}");
+    return HttpRequest.request(this.host + url,
+        method: method,
+        requestHeaders: {ElosAuthHeader: this.accessToken},
+        sendData: JSON.encode(data));
+  }
+
+  Future<HttpRequest> GET(String url, dynamic data) async {
+    return request("GET", url, data);
+  }
+
+  Future<HttpRequest> POST(String url, dynamic data) async {
+    return request("POST", url, data);
+  }
+
+  Future<HttpRequest> DELETE(String url, dynamic data) async {
+    return request("DELETE", url, data);
+  }
+}
